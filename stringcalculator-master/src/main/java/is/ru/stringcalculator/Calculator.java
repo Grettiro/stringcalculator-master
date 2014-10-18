5023712cbdbca039482e7b8bf3cccb9f38d45467
Grettiro <grettir10@ru.is>
2014-10-18 00:46:56 -0700

    A very bad mix to be able to allow multiple delimiters and any length delimiters, always did the test function first

--- stringcalculator-master/src/main/java/is/ru/stringcalculator/Calculator.java
+++ stringcalculator-master/src/main/java/is/ru/stringcalculator/Calculator.java
@@ -17,14 +17,13 @@ public class Calculator {
 			{
 				String delim = matcher.group(1);
 				String digits = matcher.group(2);
+				System.out.println("Delimiter before replace: " + delim);
 				delim = delim.replaceAll("\\[", "").replaceAll("\\]","");
-				return sum(splitNumbers2(digits, Pattern.quote(delim)));
+				//return sum(splitNumbers2(digits, Pattern.quote(delim)));
+				return sum(splitNumbers2(digits, delim));
 			}
 			else
-			{
 				return 0;
-			}
-			//return sum(splitNumbers2((text.substring(3).replaceAll("\\n", "")), text.substring(2,3)));
 		}
 		else if(text.contains(",") || text.contains("\n")){
 			return sum(splitNumbers(text));
@@ -42,7 +41,23 @@ public class Calculator {
 	}
 
 	private static String[] splitNumbers2(String numbers, String delimiter){
-			return numbers.split(delimiter);
+
+		Pattern pattern2 = Pattern.compile("\\" + delimiter.charAt(1) + "{3,}?");
+		Matcher matcher2 = pattern2.matcher(delimiter);
+		if(matcher2.matches())
+		{
+			System.out.println("Durp it matched");
+			return numbers.split(pattern2.quote(delimiter));
+		}
+		else
+		{
+			delimiter = pattern2.quote(delimiter);
+			String delimiter2 = "[";
+			delimiter2 = delimiter2 + delimiter + "]";
+			System.out.println("The delimiter:" + delimiter2);
+			return numbers.split(delimiter2);
+		}
+		
 	}
       
     private static int sum(String[] numbers) {
